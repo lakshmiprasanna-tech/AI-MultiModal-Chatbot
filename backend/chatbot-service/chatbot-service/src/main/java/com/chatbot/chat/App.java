0e0a5802f39dde48833b45e3b07c32fcf381069e
Lakshmi Prasanna Thotakura <lakshmisp.958@gmail.com>
2025-03-06 03:27:16 +0000

    Initialized microservices for chatbot

---
+++ backend/chatbot-service/chatbot-service/src/main/java/com/chatbot/chat/App.java
@@ -0,0 +1,13 @@
+package com.chatbot.chat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
